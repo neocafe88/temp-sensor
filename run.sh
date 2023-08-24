55a2eea105f22fa850cd61d273572ef19a35377a
HJ Cho <hyoun.cho@colorado.edu>
2023-08-24 13:16:10 -0500

    updated

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-printf "\nRunning temp sensor...\n"
-
-./tempsensor --endpoint arehxr1om855h-ats.iot.us-east-1.amazonaws.com --cert BBBTempSensor.cert.pem --key BBBTempSensor.private.key --ca_file root-CA.crt --client_id tempsensor --topic sdk/test/temp
-
-
---
+++ temp-sensor.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+printf "\nRunning temp sensor...\n"
+
+./tempsensor --endpoint arehxr1om855h-ats.iot.us-east-1.amazonaws.com --cert /etc/temp-sensor/BBBTempSensor.cert.pem --key /etc/temp-sensor/BBBTempSensor.private.key --ca_file /etc/temp-sensor/root-CA.crt --client_id tempsensor --topic sdk/test/temp
+
+
